40937a54ebec889e13e4905a256a2161afcc8d6e
Prasad Deshmukh <prasaddeshmukh543@gmail.com>
2022-04-12 23:29:53 +0530

    assigmentAdd project files.

---
+++ ArrayAssigmentDemo12April/Program.cs
@@ -0,0 +1,19 @@
+﻿//Demo Assigment salim 12 April 2022
+
+
+
+
+
+for (int row = 0; row < 5; row++)
+{
+    for (int column = 0; column < 5; column++)
+    {
+        if (((column == 0 || column == 4) && (row > 3 || row < 1)) || row == column || (column == 1 && row == 3) || column == 3 && row == 1)
+            Console.Write("*\t");
+        else
+            Console.Write("\t");
+    }
+    Console.Write("\n");
+}
+
+
